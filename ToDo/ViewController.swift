082837c5edb0de21965e7a5f2a71a99c4bdaa3f4
Anatoly Aleksandrov <aleksandrov_a_a@icloud.com>
2025-07-23 11:25:58 +0700

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Анатолий Александрович on 23.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ToDoTests/ToDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTests.swift
+//  ToDoTests
+//
+//  Created by Анатолий Александрович on 23.07.2025.
+//
+
+import Testing
+@testable import ToDo
+
+struct ToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
